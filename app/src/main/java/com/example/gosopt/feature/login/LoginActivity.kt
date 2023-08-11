4296c01d2a131a3a8ae3e4bbef8a25748602dded
librarywon <librarywon@naver.com>
2023-08-11 17:05:54 +0900

    [feat]: login 화면 구현

--- app/src/main/java/com/example/gosopt/feature/login/LoginActivity.kt
+++ app/src/main/java/com/example/gosopt/feature/login/LoginActivity.kt
@@ -6,6 +6,7 @@ import com.example.gosopt.R
 import com.example.gosopt.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    //by lazy를 사용하면 해당 변수가 사용될 때 초기화가 진행됩니다.
     private val binding by lazy { ActivityLoginBinding.inflate(layoutInflater) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/example/gosopt/feature/login/LoginViewModel.kt
+++ app/src/main/java/com/example/gosopt/feature/login/LoginViewModel.kt
@@ -1,7 +1,18 @@
 package com.example.gosopt.feature.login
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class LoginViewModel : ViewModel() {
+    private val _id = MutableLiveData<String>()
+    val id : LiveData<String> get() = _id
+
+    private val _pw = MutableLiveData<String>()
+    val pw : LiveData<String> get() = _pw
+
+    fun login() {
+
+    }
 
 }
